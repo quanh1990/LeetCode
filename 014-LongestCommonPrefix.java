860cd0c0f0cff50263ccc12be4fe909602e976bb
quanheng <quanheng@travelsky.com>
2017-11-07 22:17:33 +0800

    easy

--- 014-LongestCommonPrefix.java
+++ 014-LongestCommonPrefix.java
@@ -14,8 +14,11 @@ public class No014LongestCommonPrefix {
         
         if (strs.length == 0) return "";
         String prefix = strs[0];
+
         for (int i = 1; i < strs.length; i++) {
+
             while (strs[i].indexOf(prefix) != 0) {
+                
                 System.out.println(strs[i] + " " + prefix + " " + strs[i].indexOf(prefix));
                 prefix = prefix.substring(0, prefix.length() - 1);
                 if (prefix.isEmpty()) return "";
